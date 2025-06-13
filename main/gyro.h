fb392e42aaa892d4e903d178410b2993767acb83
YonatanReshef <yreshef@campus.technion.ac.il>
2025-06-13 17:24:25 +0300

    added comments to gyro

--- main/gyro.h
+++ main/gyro.h
@@ -6,8 +6,8 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-
-enum Color {
+// Sides for movement
+enum SIDE {
     LEFT,
     RIGHT,
     UP,
@@ -15,27 +15,36 @@ enum Color {
 };
 
 
+
 class Gyro
 {   
 private:
+    // MPU6050 object
     Adafruit_MPU6050 mpu;
 
+    //acceleration arrays in each axis
     double acc_x_arr[10];
     double acc_y_arr[10];
     double acc_z_arr[10];
 
+    //acceleration values in each exis
     double acc_x, acc_y, acc_z;
 
+    // sensors for mpu
     sensors_event_t a, g, temp;
 
 
-
+    // index in arrays for next reading
     int reading_idx;
 
     
-
+    // Get the average of an array
     double getArrAvg(double* arr)
+
+    // Put the next reading in the arrays
     void updateArrays();
+
+    // get the Gyro readings
     void getGyroRead();
 
 public:
