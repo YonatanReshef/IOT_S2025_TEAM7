8e8830baae2bf898a49d2db4c01bec397fb79cca
GeckoWarrior <guy.sudai2@gmail.com>
2025-06-26 15:05:02 +0300

    mazeMaps num maps

--- main/mazeMaps.h
+++ main/mazeMaps.h
@@ -14,35 +14,31 @@ public:
             BORDER = 4
     };
 
+    MazeMaps();
+    ~MazeMaps() = default;
+    
+    
+    void getMapPart(int num_screens, int map_id, int screen_id, BlockType out_map[18][18]);
+    
+    void fillBorder(int num_screens, int map_id, int other_screen_id, BoardLayout::SIDE my_side, BoardLayout::SIDE other_side, BlockType out_map[18][18]);
+    
+    //IF WE SAVE TO FILE, ADD A LOAD
+    //GENERATE ~~~~~~~~~~
+    
+    //num maps for each num of screens
+    static const int num_maps = 1;
+    
 private:
     
     /* name[num_of_maps][num_of_boards][board_widht][board_height]*/
 
-    BlockType maps_for_1[2][1][16][16];
+    BlockType maps_for_1[num_maps][1][16][16];
 
-    BlockType maps_for_2[2][2][16][16];
+    BlockType maps_for_2[num_maps][2][16][16];
 
-    BlockType maps_for_3[2][3][16][16];
+    BlockType maps_for_3[num_maps][3][16][16];
 
-    BlockType maps_for_4[2][4][16][16];
-
-
-public:
-
-    MazeMaps();
-    ~MazeMaps() = default;
-
-
-    void getMapPart(int num_screens, int map_id, int screen_id, BlockType out_map[18][18]);
-
-    void fillBorder(int num_screens, int map_id, int other_screen_id, BoardLayout::SIDE my_side, BoardLayout::SIDE other_side, BlockType out_map[18][18]);
-
-    //IF WE SAVE TO FILE, ADD A LOAD
-    //GENERATE ~~~~~~~~~~
-
-    //num maps for each num of screens
-    int num_maps = 2;
-
+    BlockType maps_for_4[num_maps][4][16][16];
 };
 
 #endif
